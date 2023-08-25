97d30eb2ae915504da22ed4ac2642d7ac39a877c
kcy230426 <purple_galaxy@naver.com>
2023-08-25 14:58:35 +0900

    js 수정

--- js/slide.js
+++ js/slide.js
@@ -1,14 +1,26 @@
 $(function(){
     var num = 0;
-    $(".slidebox li").fadeOut(0)
-    $(".slidebox li").eq(num).fadeIn(0)
+    $("#slide li").fadeOut(0);
+    $("#slide li").eq(num).fadeIn(0);
     setInterval(function(){
         if(num<2){
-            num++;
-        }else{num=0;}
-        $(".slidebox li").fadeOut('slow');
-        $(".slidebox li").eq(num).fadeIn('slow')
+            num ++;
+        }else{
+            num = 0;
+        }
+        $("#slide li").fadeOut('slow');
+        $("#slide li").eq(num).fadeIn('slow')
     },3000)
+    // var num = 0;
+    // $(".slidebox li").fadeOut(0)
+    // $(".slidebox li").eq(num).fadeIn(0)
+    // setInterval(function(){
+    //     if(num<2){
+    //         num++;
+    //     }else{num=0;}
+    //     $(".slidebox li").fadeOut('slow');
+    //     $(".slidebox li").eq(num).fadeIn('slow')
+    // },3000)
     $("body").on("click","#threeTabs .nbox li", function(){
         $(".popup").addClass("on")
     })
